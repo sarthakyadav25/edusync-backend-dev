de80ad91964c8f825668e90d450dd6087d6a1c16
Ashish Ranjan Singh <singh.ashishhhh@gmail.com>
2026-01-15 18:50:31 +0530

    feat(iam): add role lookup helpers to repository

--- edusync/src/main/java/com/project/edusync/iam/repository/RoleRepository.java
+++ edusync/src/main/java/com/project/edusync/iam/repository/RoleRepository.java
@@ -7,5 +7,10 @@ import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
-    Role findByName(String roleStudent);
+    /**
+     * Finds a Role entity by its unique name (e.g., "SCHOOL_ADMIN", "STUDENT").
+     * Spring Data JPA automatically generates the SQL query:
+     * SELECT * FROM roles WHERE name = ?
+     */
+    Optional<Role> findByName(String name);
 }
