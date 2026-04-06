1ee66e220698c2ac95272dbf31c465bd57017ee3
sarthak <sarthakvyadav@gmail.com>
2026-04-06 17:01:25 +0530

    Added: Endpoints for teacher

---
+++ src/main/java/com/project/edusync/teacher/service/LectureLogService.java
@@ -0,0 +1,14 @@
+package com.project.edusync.teacher.service;
+
+import com.project.edusync.teacher.model.dto.LectureLogRequestDto;
+import com.project.edusync.teacher.model.dto.LectureLogResponseDto;
+import com.project.edusync.teacher.model.dto.LectureLogUploadInitRequestDto;
+import com.project.edusync.teacher.model.dto.LectureLogUploadInitResponseDto;
+
+import java.util.UUID;
+
+public interface LectureLogService {
+    LectureLogResponseDto getLectureLog(Long teacherId, UUID scheduleUuid);
+    LectureLogResponseDto saveLectureLog(Long teacherId, LectureLogRequestDto requestDto);
+    LectureLogUploadInitResponseDto initUpload(Long teacherId, LectureLogUploadInitRequestDto request);
+}
